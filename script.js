e37eb7694d010a349223f161a9315e9106764ac2
Honey-raj-55 <honeyfx505@gmail.com>
2020-09-17 23:01:55 -0700

    Responsive-website

---
+++ script.js
@@ -0,0 +1,21 @@
+
+var imgs = [
+ 
+   "Pets/Dog-1.jpg",
+   "Pets/Puppy-2.jpg",
+   "Pets/Puppy-3.jpg",
+   "Pets/Dog-4.jpg"
+         
+];
+
+var button = document.querySelector("button"); 
+var img = document.querySelector("img");
+var count = 1;
+
+button.addEventListener("click", function(){ 
+    if (count === 4) {
+        count =0;
+    }
+    img.src = imgs[count]
+    count = count +1;
+});
