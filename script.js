4c46aa70a5dcdf242d73677092568a842912d639
Honey raj <71264544+Honey-raj-55@users.noreply.github.com>
2021-10-15 00:49:18 -0700

    updated

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ var imgs = [
    "Pets/Dog-1.jpg",
    "Pets/Puppy-2.jpg",
    "Pets/Puppy-3.jpg",
-   "Pets/Dog-4.jpg"
+   "Pets/Dog-4.jpg",
          
 ];
 
@@ -14,7 +14,7 @@ var count = 1;
 
 button.addEventListener("click", function(){ 
     if (count === 4) {
-        count =0;
+        count = 0;
     }
     img.src = imgs[count]
     count = count +1;
